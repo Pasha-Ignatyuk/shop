474450139589fa0e5f5afb17ab3918b3e59628f6
Pasha-Ignatyuk <kiotocloud@gmail.com>
2022-01-17 21:19:39 +0300

    first commit

---
+++ shop/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from django.urls import path, re_path
+from . import views
+app_name = 'shop'
+urlpatterns = [
+    path('', views.product_list, name='product_list'),
+    path('<slug:category_slug>/', views.product_list,
+         name='product_list_by_category'),
+    path('<int:id>/<slug:slug>/', views.product_detail,
+         name='detail'),
+]
